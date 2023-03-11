043cca82eae7a06d1b27079b7394376d6e1afe03
cberletch <65048267+cberletch@users.noreply.github.com>
2023-03-10 22:33:59 -0500

    added stars and lighting

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -17,12 +19,33 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial ( { color: 'white', wireframe: true });
+const material = new THREE.MeshStandardMaterial ( { color: 'white' });
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus)
 
+const pointLight = new THREE.PointLight(0xfffff)
+pointLight.position.set(5,5,5)
+
+scene.add(pointLight)
+
 renderer.render( scene, camera );
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 'white' });
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 )); 
+
+  star.position.set(x, y ,z);
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
 
 function animate() {
   requestAnimationFrame( animate );
@@ -30,6 +53,8 @@ function animate() {
   torus.rotation.x += .01;
   torus.rotation.y += .005;
   torus.rotation.z += .0075;
+
+  controls.update();
 
   renderer.render( scene, camera );
 }
